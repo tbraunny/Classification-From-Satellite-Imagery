a05b14a6146e8d0e5f5880db18039eb18558f6df
tbraunny <tbraun@unr.edu>
2024-11-06 22:44:01 -0800

    model successful 97% accuracy

--- cnn.py
+++ cnn.py
@@ -1,109 +1,141 @@
 import numpy as np
 import pandas as pd
 import torch
-import torch as nn
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import os
 import cv2
 from torch.utils.data import TensorDataset , DataLoader
 from glob import glob
-from sklearn import train_test_split
-
+from sklearn.model_selection import train_test_split
 
 # load dataset, return images & labels as numpy arrays
 def load_dataset():
-    basepath = 'C:\\Undergraduate\\Year 4\\CS 482\\final_project\\planesnet\\planesnet'
-    images , labels = [] , []
 
-    # loop thru each class to find all files with name starting with label_*
-    for label in range(2):
-        imgs_path = os.path.join(basepath , f"{label}_*")
+    # if planesnet_tensors.pt exists, load those instead
+    if os.path.exists("planesnet_tensors.pt"):
+        # Load the tensors
+        print(f"Loading tensor data...")
+        data = torch.load("planesnet_tensors.pt" , weights_only=False)
+        images, labels = data[0], data[1]    
+    else:
+        print("Loading image data...")
+        basepath = 'C:\\Undergraduate\\Year 4\\CS 482\\final_project\\planesnet\\planesnet'
+        #basepath = '/content/drive/MyDrive/planesnet/'
+        images , labels = [] , []
 
-        # prep image for addition to list
-        for file in imgs_path:
-            img = cv2.imread(file)
-            img = cv2.cvtColor(img , cv2.COLOR_BGR2RGB) # convert BGR to RGB
-            images.append(img)
-            labels.append(label)
+        # loop thru each class to find all files with name starting with label_*
+        for label in range(2):
+            imgs_path = os.path.join(basepath , f"{label}_*")
 
-    # convert to np arrays for easier handling
-    images = np.array(images , dtpye=np.int64)
-    labels = np.array(labels , dtpye=np.int64)
+            # prep image for addition to list
+            for file in glob(imgs_path):
+                img = cv2.imread(file)
+                img = cv2.cvtColor(img , cv2.COLOR_BGR2RGB) # convert BGR to RGB
+                images.append(img)
+                labels.append(label)
+
+            print(f"{label}\t{len(images)}")
+
+        # convert to np arrays for easier handling
+        images = np.array(images , dtype=np.int64)
+        labels = np.array(labels , dtype=np.int64)
+
+        images_tensor = torch.tensor(images)
+        labels_tensor = torch.tensor(labels)
+
+        # Save tensors to a file
+        torch.save((images_tensor, labels_tensor), f'./planesnet_tensors.pt')
 
     return images , labels
 
 def data_preprocess(X , y):
     X_train , X_test , y_train , y_test = train_test_split(X , y , test_size=0.3 , random_state=42)
 
-    X_train , X_test = torch.tensor(X_train) , torch.tensor(X_test)
-    y_train , y_test = torch.tensor(y_train) , torch.tensor(y_test)
-
     train_data = TensorDataset(X_train , y_train)
     test_data = TensorDataset(X_test , y_test)
 
-    train_dataloader = DataLoader(train_data , batch_size=64 , shuffle=True)
-    test_dataloader = DataLoader(test_data , batch_size=64 , shuffle=True)
+    train_dataloader = DataLoader(train_data , batch_size=256 , shuffle=True)
+    test_dataloader = DataLoader(test_data , batch_size=256 , shuffle=True)
 
     return train_dataloader , test_dataloader
 
 # define model network
-class CNN():
+class CNN(nn.Module):
     def __init__(self):
-        super(self , CNN).__init__()
+        super(CNN , self).__init__()
 
         # layers + dropout
-        self.conv1 = nn.conv2d(1 , 32 , 3 , 1)
-        self.conv2 = nn.conv2d(32 , 64 , 3 , 1)
-        self.fc1 = nn.Linear(9216 , 128)
+        self.conv1 = nn.Conv2d(20 , 32 , kernel_size=3 , stride=1 , padding=1)
+        self.conv2 = nn.Conv2d(32 , 64 , kernel_size=3 , stride=1 , padding=1)
+        self.fc1 = nn.Linear(640 , 128)
         self.fc2 = nn.Linear(128 , 2)
-        self.dropout1 = nn.dropout(0.25)
-        self.dropout2 = nn.dropout(0.5)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
 
     def forward(self , x):
         x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x , 2)
+        x = F.leaky_relu(x)
+        x = self.bn1(x)
         x = self.dropout1(x)
-        x = nn.flatten(x , 1)
+
+        x = self.conv2(x)
+        x = F.leaky_relu(x)
+        x = self.bn2(x)
+        x = F.max_pool2d(x , 2)
+
+        x = x.view(x.size(0) , -1) # Flatten the tensor
+
         x = self.fc1(x)
         x = F.relu(x)
         x = self.dropout2(x)
+
         x = self.fc2(x)
-        output = F.log_softmax(x)
+        output = F.log_softmax(x , dim=1)
 
         return output
 
 
 # train model
-def train_cnn(X_train , y_train):
-    cnn = CNN()
+def train_cnn(train_loader , test_loader , device):
+    cnn = CNN().to(device)
 
     # hyperparameters
-    num_epochs = 100
-    learning_rate = 0.001
-    criterion = F.nll_loss() # placeholder
-    optimizer = nn.optim.Adam(cnn.parameters() , learning_rate)
-    train_losses = []
+    num_epochs = 10
+    learning_rate = 0.01
+    #criterion = F.nll_loss()
+    optimizer = optim.Adam(cnn.parameters() , learning_rate)
+    train_losses , test_losses = [] , []
 
     for epoch in range(num_epochs):
         cnn.train()
-        optimizer.zero_grad()
-        outputs = cnn(X_train.float())
-        loss = criterion(outputs , y_train)
-        loss.backward()
-        optimizer.step()
-        train_losses.append(loss.item()) # store loss
+        running_loss = 0
+        for batch_idx , (data , target) in enumerate(train_loader):
+          optimizer.zero_grad()
+          outputs = cnn(data.float())
+          loss = F.nll_loss(outputs , target)
+          loss.backward()
+          optimizer.step()
+          running_loss += loss.item()
 
-    y_pred = []
-    return y_pred
+        train_losses.append(running_loss) # store loss
+        print(f"Epoch {epoch + 1} \t Loss: {running_loss}")
+      
+    # predict
+    num_correct = 0
+    cnn.eval()
+    with torch.no_grad():
+      for (data , target) in test_loader:
+        output = cnn(data.float())
+        loss = F.nll_loss(output , target)
+        test_losses.append(loss.item())
+        pred = output.argmax(dim=1, keepdim=True)
+        num_correct += pred.eq(target.view_as(pred)).sum().item()
 
-
-def evaluate(pred):
-    accuracy = 0
-    return accuracy
+    return num_correct / len(test_loader.dataset)
 
 '''
 def save_model():
@@ -116,7 +148,11 @@ def save_model():
 '''
 
 if __name__ == '__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("device: " , device)
     images , labels = load_dataset()
     train , test = data_preprocess(images , labels)
-    predictions = train_cnn(test)
-    accuracy = evaluate(predictions)
+    accuracy = train_cnn(train , test , device)
+    print("Accuracy: " , accuracy)
+
+    #save_model()
