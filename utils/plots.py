5af6a703b66739a78d419e37dc92762427366657
tbraunny <tbraun@unr.edu>
2024-12-08 15:42:21 -0800

    reconfigured graphs

--- utils/plots.py
+++ utils/plots.py
@@ -2,29 +2,32 @@ import matplotlib.pyplot as plt
 import os
 
 # plot loss & accuracy, save to results
-def plot(train_loss , test_loss , train_accuracy , test_accuracy , model):
+def plot(x1 , x2 , model , mode):
     save_dir = "results"
 
-    # loss
-    plt.plot(train_loss , color='blue' , label="Train")
-    plt.plot(test_loss , color='red' , label="Test")
-    plt.xlabel("Epochs")
-    plt.ylabel("Loss")
-    plt.title("Epochs vs. Loss")
-    plt.legend()
+    data1 = [item[0] for item in x1]
+    epochs1 = [item[1] for item in x1] 
+    data2 = [item[0] for item in x2]
+    epochs2 = [item[1] for item in x2] 
 
-    loss_path = os.path.join(save_dir , f'loss_plot_{model.__name__}.png')
+    # general
+    plt.figure()
+    plt.plot(epochs1 , data1 , color='blue' , label="Train")
+    plt.plot(epochs2 , data2 , color='red' , label="Test")
+    plt.xlabel("Epochs")
+    plt.ylabel(mode)
+    plt.title(f'Epochs vs. {mode} {model.__name__}')
+    plt.legend()
+    loss_path = os.path.join(save_dir , f'{mode}_plot_{model.__name__}.png')
     plt.savefig(loss_path)
-    plt.show()
 
-    # accuracy
-    plt.plot(train_accuracy , color='blue' , label="Train")
-    plt.plot(test_accuracy , color='red' , label="Test")
-    plt.xlabel("Epochs")
-    plt.ylabel("Accuracy")
-    plt.title("Epochs vs. Accuracy")
-    plt.legend()
-
-    accuracy_path = os.path.join(save_dir , f'accuracy_plot_{model.__name__}.png')
-    plt.savefig(accuracy_path)
-    plt.show()
+    # # accuracy
+    # plt.figure()
+    # plt.plot(train_accuracy , color='blue' , label="Train Accuracy")
+    # plt.plot(test_accuracy , color='red' , label="Test Accuracy")
+    # plt.xlabel("Epochs")
+    # plt.ylabel("Accuracy")
+    # plt.title(f'Epochs vs. Accuracy {model.__name__}')    
+    # plt.legend()
+    # accuracy_path = os.path.join(save_dir , f'accuracy_plot_{model.__name__}.png')
+    # plt.savefig(accuracy_path)
