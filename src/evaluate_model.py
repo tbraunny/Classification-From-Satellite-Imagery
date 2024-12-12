ca5ad72baef54ec2d3475330d1f6292aef96feaf
tbraunny <tbraun@unr.edu>
2024-12-12 11:03:44 -0800

    minor changes

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -4,12 +4,44 @@ import utils.load_model
 import cnn
 import log_reg
 import xgb
+import argparse
+
+parser = argparse.ArgumentParser(description="Evaluate a model with specific configurations.")
+
+# Add arguments for model selection and scene number
+parser.add_argument(
+    "--model_selection",
+    type=int,
+    choices=[1, 2, 3],
+    required=True,
+    help="Specify which model to run: 1 for xgboost, 2 for log_reg, 3 for cnn."
+)
+parser.add_argument(
+    "--scene_num",
+    type=int,
+    choices=[1, 2, 3, 4, 5],
+    required=True,
+    help=(
+        "Specify the scene number:\n"
+        "1: San Francisco Intl (SFO)\n"
+        "2: San Diego Intl (SAN)\n"
+        "3: Los Angeles Intl (LAX)\n"
+        "4: Southern California Logistics (SCLA) Airport Boneyard\n"
+        "5: New Jersey Coast (Nov 2021) 39°28'20.2\"N 74°18'49.7\"W (15,000ft)"
+    )
+)
+
+args = parser.parse_args()
+
+# Access the arguments
+model_selection = args.model_selection
+scene_num = args.scene_num
 
 # specify which model you would like to run:
 # - 1 : xgboost
 # - 2 : log_reg
 # - 3 : cnn
-model_selection = 3
+#model_selection = 3
 
 # scene numbers:
 # - 1 : San Francisco Intl (SFO)
@@ -17,7 +49,7 @@ model_selection = 3
 # - 3 : Los Angeles Intl (LAX)
 # - 4 : Southern California Logistics (SCLA) Airport Boneyard 
 # - 5 : New Jersey Coast (Nov 2021) 39°28'20.2"N 74°18'49.7"W (15,000ft)
-scene_num = 1
+#scene_num = 1
 
 ############################################
 stride = 3
